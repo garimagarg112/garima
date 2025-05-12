4498331fdebea41d03569fb334f8040ad1da6842
garimagarg112 <garima.garg112@gmail.com>
2025-05-12 18:18:20 +0530

    add file

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
-     allowedHosts: 'all'
+     allowedHosts: 'garima-xe2b.onrender.com'
   }
 })
